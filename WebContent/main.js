4bcc59e3e2c16181e858c8fa47f24a7810b6897d
Niven Francis <nivenf@email.arizona.edu>
2018-06-09 12:51:53 -0700

    Add files via upload

--- WebContent/main.js
+++ WebContent/main.js
@@ -8,18 +8,37 @@ var svg_dir = d3.select("#director")
 				.append("svg")
 				.attr("width", window.innerWidth - 4)
 				.attr("height", window.innerHeight - 4)
-				.attr("id", "d3_director");
+				.attr("id", "d3_director")
+				.on("mousemove", function() {
+					svg_dir.selectAll("image,text")
+						.attr("transform", function() {
+							var x_offset = d3.event.pageX * -0.02 + (WIN_WIDTH*0.02/2);
+							var y_offset = d3.event.pageY * -0.02 + (WIN_HEIGHT*0.02/2);
+							
+							
+							return "translate(" + x_offset + "," + y_offset + ")"
+						});
+				})
 
 var div = d3.select("body").append("div")
 .attr("class", "tooltip")
-.style("display", "none");
-var tooltip_flag = 0;
+.style("display", "none")
+.attr("id", "tt");
+var tooltip_text = d3.select("#tt").html("<span id=\"tt_text\"></span>");
+
+/*
+ * 	+-------------------+
+ * 	|		PLANETS		|
+ * 	+-------------------+
+ */
+
+
 
 // SATURN
 var d_sa_cx = WIN_SCALE - WIN_SCALE * 150;
 var d_sa_cy = WIN_HEIGHT - WIN_SCALE * 890;
 
-var destination_saturn = svg_dir.append("svg:image")
+var destination_saturn = svg_dir.append("image")
 .attr("x", function() { return d_sa_cx; })
 .attr("y", function() { return d_sa_cy; })
 .attr("width", 900 * WIN_SCALE)
@@ -27,12 +46,16 @@ var destination_saturn = svg_dir.append("svg:image")
 .attr("xlink:href", "img/saturn.png");
 
 
+
+
+
+
 // JUPITER
 var d_ju_cx = WIN_WIDTH - 900 * WIN_SCALE + 150;
 var d_ju_cy = WIN_HEIGHT - 596 * WIN_SCALE + 50;
 var d_ju_r = WIN_SCALE;
 
-var destination_jupiter = svg_dir.append("svg:image")
+var destination_jupiter = svg_dir.append("image")
 .attr("x", function() { return d_ju_cx; })
 .attr("y", function() { return d_ju_cy; })
 .attr("width", 900 * WIN_SCALE)
@@ -40,12 +63,16 @@ var destination_jupiter = svg_dir.append("svg:image")
 .attr("xlink:href", "img/jupiter.png");
 
 
+
+
+
+
 // NESSUS ORBIT
 var d_no_cx = WIN_WIDTH - WIN_SCALE * 858;
 var d_no_cy = WIN_HEIGHT/2 - WIN_SCALE * 524;
 var d_no_r = WIN_SCALE * 200;
 
-var destination_nessusorbit = svg_dir.append("svg:image")
+var destination_nessusorbit = svg_dir.append("image")
 .attr("x", function() { return d_no_cx; })
 .attr("y", function() { return d_no_cy; })
 .attr("width", 768 * WIN_SCALE)
@@ -54,8 +81,12 @@ var destination_nessusorbit = svg_dir.append("svg:image")
 .attr("xlink:href", "img/levi.png");
 
 
+
+
+
+
 //LEVIATHAN
-var destination_nessusorbit = svg_dir.append("svg:image")
+var destination_nessusorbit = svg_dir.append("image")
 .attr("x", function() { return d_no_cx + 658/2 * WIN_SCALE; })
 .attr("y", function() { return d_no_cy + 433/4 * WIN_SCALE; })
 .attr("width", 120 * WIN_SCALE)
@@ -69,7 +100,6 @@ var destination_nessusorbit = svg_dir.append("svg:image")
 	d3.select("#text-leviathan").transition().duration(200)
 	.attr("fill", "white");
 	
-	tooltip_flag = 1;
 	tooltip_info("Leviathan");
 	
 	div.style("display", "inline");
@@ -83,11 +113,11 @@ var destination_nessusorbit = svg_dir.append("svg:image")
 	d3.select("#text-leviathan").transition().duration(200)
 	.attr("fill", "lightgray");
 	
-	tooltip_flag = 0;
 	div.style("display", "none");
 });
 
-var text_traveler = svg_dir.append("text")
+
+var text_leviathan = svg_dir.append("text")
 .attr("x", d_no_cx + 990/2 * WIN_SCALE)
 .attr("y", d_no_cy + 573/4 * WIN_SCALE)
 .text("NESSUS ORBIT")
@@ -98,12 +128,16 @@ var text_traveler = svg_dir.append("text")
 .attr("id", "text-leviathan");
 
 
+
+
+
+
 // THE TRAVELER
 var d_tr_cx = WIN_WIDTH/2;
 var d_tr_cy = WIN_HEIGHT/2;
 var d_tr_r = WIN_SCALE * 80;
 
-var destination_traveler_bg = svg_dir.append("svg:image")
+var destination_traveler_bg = svg_dir.append("image")
 .attr("x", function() { return d_tr_cx - (480/2 * WIN_SCALE); })
 .attr("y", function() { return d_tr_cy - (480/2 * WIN_SCALE); })
 .attr("width", function() { return 480 * WIN_SCALE; })
@@ -111,7 +145,8 @@ var destination_traveler_bg = svg_dir.append("svg:image")
 .attr("opacity", 0.8)
 .attr("xlink:href", "img/traveler.png");
 
-var destination_traveler = svg_dir.append("svg:image")
+
+var destination_traveler = svg_dir.append("image")
 .attr("x", function() { return d_tr_cx - (146/2 * WIN_SCALE); })
 .attr("y", function() { return d_tr_cy - (146/2 * WIN_SCALE); })
 .attr("width", function() { return 146 * WIN_SCALE; })
@@ -143,6 +178,7 @@ var destination_traveler = svg_dir.append("svg:image")
 	div.style("display", "none");
 });
 
+
 var text_traveler = svg_dir.append("text")
 .attr("x", d_tr_cx + d_tr_r * 1.5)
 .attr("y", d_tr_cy)
@@ -154,16 +190,60 @@ var text_traveler = svg_dir.append("text")
 .attr("id", "text-traveler");
 
 
+
+
+
+
 // EARTH
-var d_ea_cx = WIN_WIDTH/2;
-var d_ea_cy = WIN_HEIGHT - WIN_SCALE * 180;
+var d_ea_cx = WIN_WIDTH/2 - (314/2 * WIN_SCALE);
+var d_ea_cy = WIN_HEIGHT - WIN_SCALE * 314/2 - WIN_SCALE * 190	;
 var d_ea_r = WIN_SCALE * 170;
 
-var destination_earth = svg_dir.append("circle")
-.attr("cx", function() { return d_ea_cx; })
-.attr("cy", function() { return d_ea_cy; })
-.attr("r", function() { return d_ea_r; })
-.attr("fill", function() { return "green"; });
+var destination_earth = svg_dir.append("image")
+.attr("x", function() { return d_ea_cx; })
+.attr("y", function() { return d_ea_cy; })
+.attr("xlink:href", "img/earth.png")
+.attr("width", 314 * WIN_SCALE)
+.attr("height", 314 * WIN_SCALE)
+.attr("opacity", 0.8)
+.on("mouseover", function() {
+	d3.select(this).transition().duration(200)
+	  .attr("opacity", 1);
+	d3.select("#text-earth").transition().duration(200)
+	.attr("fill", "white");
+	
+	tooltip_flag = 1;
+	tooltip_info("Earth");
+	
+	div.style("display", "inline");
+})
+.on("mousemove", function() {
+	tooltip_display();
+})
+.on("mouseout", function() {
+	d3.select(this).transition().duration(200)
+	  .attr("opacity", 0.8);
+	d3.select("#text-earth").transition().duration(200)
+	.attr("fill", "lightgray");
+
+	tooltip_flag = 0;
+	div.style("display", "none");
+});
+
+
+var text_earth = svg_dir.append("text")
+.attr("x", d_ea_cx + WIN_SCALE * 385)
+.attr("y", d_ea_cy + WIN_SCALE * 120)
+.text("EARTH")
+.attr("font-family", "'EB Garamond', serif")
+.attr("font-size", "16px")
+.attr("letter-spacing", "12px")
+.attr("fill", "lightgray")
+.attr("id", "text-earth");
+
+
+
+
 
 
 // MERCURY
@@ -171,7 +251,7 @@ var d_me_cx = WIN_WIDTH/2  - (348/2 * WIN_SCALE);
 var d_me_cy = WIN_SCALE * 230  - (349/2 * WIN_SCALE);
 var d_me_r = WIN_SCALE * 60;
 
-var destination_mercury = svg_dir.append("svg:image")
+var destination_mercury = svg_dir.append("image")
 .attr("x", function() { return d_me_cx; })
 .attr("y", function() { return d_me_cy; })
 .attr("xlink:href", "img/mercury.png")
@@ -182,7 +262,8 @@ var destination_mercury = svg_dir.append("svg:image")
 var d_me2_cx = WIN_WIDTH/2  - (130/2 * WIN_SCALE);
 var d_me2_cy = WIN_SCALE * 230  - (172/2 * WIN_SCALE);
 
-var destination_mercury = svg_dir.append("svg:image")
+
+var destination_mercury = svg_dir.append("image")
 .attr("x", function() { return d_me2_cx; })
 .attr("y", function() { return d_me2_cy; })
 .attr("xlink:href", "img/mercury_planet.png")
@@ -213,6 +294,7 @@ var destination_mercury = svg_dir.append("svg:image")
 	div.style("display", "none");
 });
 
+
 var text_mercury = svg_dir.append("text")
 .attr("x", d_me2_cx*1.18)
 .attr("y", d_me2_cy*1.69)
@@ -224,16 +306,35 @@ var text_mercury = svg_dir.append("text")
 .attr("id", "text-mercury");
 
 
+
+
+
+
 // MARS
-var d_ma_cx = WIN_SCALE * 350;
-var d_ma_cy = WIN_HEIGHT/2 - WIN_SCALE * 50;
+var d_ma_cx = WIN_SCALE * 224;
+var d_ma_cy = WIN_HEIGHT/2 - WIN_SCALE * 310/2;
 var d_ma_r = WIN_SCALE * 120;
 
-var destination_mars = svg_dir.append("circle")
-.attr("cx", function() { return d_ma_cx; })
-.attr("cy", function() { return d_ma_cy; })
-.attr("r", function() { return d_ma_r; })
-.attr("fill", function() { return "red"; });
+var destination_mars = svg_dir.append("image")
+.attr("x", function() { return d_ma_cx; })
+.attr("y", function() { return d_ma_cy; })
+.attr("xlink:href", "img/mars.png")
+.attr("width", 224 * WIN_SCALE)
+.attr("height", 224 * WIN_SCALE)
+.attr("opacity", 0.8);
+
+
+var text_mars = svg_dir.append("text")
+.attr("x", d_ma_cx*2 + WIN_SCALE * 80)
+.attr("y", d_ma_cy*1.5 - WIN_SCALE * 80)
+.text("MARS")
+.attr("font-family", "'EB Garamond', serif")
+.attr("font-size", "16px")
+.attr("letter-spacing", "12px")
+.attr("fill", "lightgray")
+.attr("id", "text-mars");
+
+
 
 
 // TITAN
@@ -248,6 +349,10 @@ var destination_titan = svg_dir.append("circle")
 .attr("fill", function() { return "blue"; });
 
 
+
+
+
+
 // IO
 var d_io_cx = WIN_WIDTH - WIN_SCALE * 400;
 var d_io_cy = WIN_HEIGHT - WIN_SCALE * 300;
@@ -258,6 +363,10 @@ var destination_io = svg_dir.append("circle")
 .attr("cy", function() { return d_io_cy; })
 .attr("r", function() { return d_io_r; })
 .attr("fill", function() { return "yellow"; });
+
+
+
+
 
 
 // NESSUS
@@ -272,6 +381,10 @@ var destination_nessus = svg_dir.append("circle")
 .attr("fill", function() { return "teal"; });
 
 
+
+
+
+
 // CRUCIBLE
 var d_cr_cx = WIN_SCALE * 630;
 var d_cr_cy = WIN_SCALE * 260;
@@ -282,6 +395,10 @@ var destination_crucible = svg_dir.append("circle")
 .attr("cy", function() { return d_cr_cy; })
 .attr("r", function() { return d_cr_r; })
 .attr("fill", function() { return "darkred"; });
+
+
+
+
 
 
 // STRIKES
@@ -295,14 +412,25 @@ var destination_strikes = svg_dir.append("circle")
 .attr("r", function() { return d_st_r; })
 .attr("fill", function() { return "darkblue"; });
 
+
+
+
+/*
+ * 	+-----------------------+
+ * 	|		FUNCTIONS		|
+ * 	+-----------------------+
+ */
+
+
 function tooltip_display() {
 	  div
       .style("left", function() {
     	  var x_coor = d3.event.pageX;
     	  var width = div.node().getBoundingClientRect().width;
+    	  console.log(width);
     	  
     	  if(width + x_coor < WIN_WIDTH - 50) {
-    		  return (d3.event.pageX) + "px";
+    		  return (d3.event.pageX + 15) + "px";
     	  }
     	  else {
     		  return (d3.event.pageX - width) + "px";
@@ -310,14 +438,36 @@ function tooltip_display() {
       })
       .style("top", function() { 
     	  var y_coor = d3.event.pageY;
-    	  return (d3.event.pageY + 20) + "px";
+    	  var height = div.node().getBoundingClientRect().height;
+    	  
+    	  if(height + y_coor < WIN_HEIGHT - 50) {
+    		  return (d3.event.pageY + 20) + "px";
+    	  }
+    	  else {
+    		  return (d3.event.pageY - height + 20) + "px";
+    	  }
       });
 }
 
 function tooltip_info(planet) {
-	div.selectAll("text").remove();
-	if(tooltip_flag == 1) {
-		div.append("text")
-	      .text(planet + " Milestones");
-	}
+	var title = "<h2>NIGHTFALL: THE PYRAMIDION</h2>";
+	var descriptor = "<h3>" + planet + "</h3>";
+	
+	var icon_div = "<div class=\"icon_header\">" + "</div>";
+	var header_div = "<div class=\"box_header\">" + title + descriptor + "</div>"
+	
+	var header = "<h4>The Crucible</h4>";
+	var description = "\"We have no Tower and no City to support us. But we can always" +
+			"drill Control. Welcome back to the Crucible, Guardians.\" - Shaxx" +
+			"This Beta playlist is a subset of the full Quickplay offering and contains the Control" +
+			"match type on the Endless Vale map." +
+			"<br> - 4v4 Low Intensity" +
+			"<br> - Firetime: 1-4 Players" +
+			"<br> - Xbox Live Required" +
+			"<br> - Level Advantages Disabled"
+	
+	var str = icon_div + header_div + header + description;
+	
+	d3.select("#tt_text")
+	.html(str);
 }
